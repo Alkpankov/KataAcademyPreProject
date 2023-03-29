6b0e02283009b78d31348a3e71fd78f72bba4a33
Alexander Punkov <alkpankov@gmail.com>
2023-03-29 16:19:24 +0300

    Spring commit. Task 2.1.4.

---
+++ src/main/java/jm/task/core/spring/intro_spring/task_2_1_4/config/AppConfig.java
@@ -0,0 +1,11 @@
+package jm.task.core.spring.intro_spring.task_2_1_4.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("jm.task.core.spring.intro_spring.task_2_1_4")
+public class AppConfig {
+
+
+}
---
+++ src/main/java/jm/task/core/spring/intro_spring/task_2_1_4/model/Animal.java
@@ -0,0 +1,4 @@
+package jm.task.core.spring.intro_spring.task_2_1_4.model;
+
+public abstract class Animal {
+}
