fd413bfab41879b97a5425e35c027b0cd46365c2
Alexander Punkov <alkpankov@gmail.com>
2023-03-29 15:00:32 +0300

    Spring commit. Task 2.1.3.

---
+++ src/main/java/jm/task/core/spring/intro_spring/Task_2_1_3/HelloWorld.java
@@ -0,0 +1,15 @@
+package jm.task.core.spring.intro_spring.Task_2_1_3;
+
+public class HelloWorld {
+ 
+    private String message;
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+     
+}
